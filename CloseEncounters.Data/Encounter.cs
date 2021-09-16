d62f5842d430fc8e81f724ee52eb85fe9f21934b
Juan Gomez <juan.c.gomez574@gmail.com>
2021-09-16 18:34:44 -0400

    Created Encounter table

---
+++ CloseEncounters.Data/Encounter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloseEncounters.Data
+{
+    public class Encounter
+    {
+        [Key]
+        public int EncounterId { get; set; }
+        [Required]
+        public string DescriptionOfEncounter { get; set; }
+        [Required]
+        public DateTime DateOfEncounter { get; set; }
+        [Required]
+        public Guid AuthorId { get; set; }
+        [Required]
+        public int CreatureId { get; set; }
+        [Required]
+        public string Location { get; set; }
+    }
+}
