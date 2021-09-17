d8ef524b1ebfde02de491f485fbb30528d0d44a2
Juan Gomez <juan.c.gomez574@gmail.com>
2021-09-16 21:22:39 -0400

    Created One Encounter model, EncounterServices, and EncounterController

---
+++ CloseEncounters.Services/EncounterService.cs
@@ -0,0 +1,101 @@
+﻿using CloseEncounters.Data;
+using CloseEncounters.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloseEncounters.Services
+{
+    public class EncounterService
+    {
+        private readonly Guid _userId;
+
+        public EncounterService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateEncounter(EncounterCreate model)
+        {
+            var entity =
+                new Encounter()
+                {
+                    AuthorId = _userId,
+                    CreatureId = model.CreatureId,
+                    DescriptionOfEncounter = model.DescriptionOfEncounter,
+                    LocationId = model.LocationId,
+                    DateOfEncounter = model.DateOfEncounter
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Encounters.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<EncounterListItem> GetEncounters()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Encounters
+                        .Where(e => e.AuthorId == _userId)
+                        .Select(
+                            e =>
+                                new EncounterListItem
+                                {
+                                    EncounterId = e.EncounterId,
+                                    CreatureId = e.CreatureId,
+                                    DateOfEncounter = e.DateOfEncounter,
+                                    DescriptionOfEncounter = e.DescriptionOfEncounter,
+                                    LocationId = e.LocationId
+                                }
+                                );
+
+                return query.ToArray();
+
+            }
+        }
+
+        public bool UpdateEncounter(EncounterEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Encounters
+                        .Single(e => e.EncounterId == model.EncounterId && e.AuthorId == _userId);
+
+                entity.EncounterId = model.EncounterId;
+                entity.DescriptionOfEncounter = model.DescriptionOfEncounter;
+                entity.CreatureId = model.CreatureId;
+                entity.DateOfEncounter = model.DateOfEncounter;
+                entity.LocationId = model.LocationId;
+
+                return ctx.SaveChanges() == 1;
+            }
+
+        }
+
+        public bool DeleteEncounter(int encounterId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Encounters
+                        .Single(e => e.EncounterId == encounterId);
+
+                ctx.Encounters.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+    }
+}
+
