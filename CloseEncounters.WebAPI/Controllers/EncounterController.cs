4663b39115e48062de7b3f8b4a77c5c0a4f78b57
Juan Gomez <juan.c.gomez574@gmail.com>
2021-09-28 18:29:58 -0400

    Added XML Documentation for Encounter Database

--- CloseEncounters.WebAPI/Controllers/EncounterController.cs
+++ CloseEncounters.WebAPI/Controllers/EncounterController.cs
@@ -12,27 +12,43 @@ namespace CloseEncounters.WebAPI.Controllers
 {
     public class EncounterController : ApiController
     {
+        /// <summary>
+        /// Returns all encounters that the current User has created.
+        /// </summary>
+        /// <returns>Encounter Object(s)</returns>
         public IHttpActionResult Get()
         {
             var eService = CreateEncounterService();
             var encounters = eService.GetEncounters();
             return Ok(encounters);
         }
-
+        /// <summary>
+        /// Returns all encounters that the specific Author Id has created.
+        /// </summary>
+        /// <param name="authorId"></param>
+        /// <returns>Encounter Object(s)</returns>
         public IHttpActionResult Get(Guid authorId)
         {
             EncounterService encounterService = CreateEncounterService();
             var encounter = encounterService.GetEncounterByAuthorId(authorId);
             return Ok(encounter);
         }
-
+        /// <summary>
+        /// Returns the encounter with the specific Encounter Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Encounter Object</returns>
         public IHttpActionResult Get(int id)
         {
             EncounterService encounterService = CreateEncounterService();
             var encounter = encounterService.GetEncounterByEncounterId(id);
             return Ok(encounter);
         }
-
+        /// <summary>
+        /// Creates a encounter object and saves it to the database.
+        /// </summary>
+        /// <param name="encounter"></param>
+        /// <returns>IHttpActionResult</returns>
         public IHttpActionResult Post(EncounterCreate encounter)
         {
             if (!ModelState.IsValid)
@@ -45,7 +61,11 @@ namespace CloseEncounters.WebAPI.Controllers
 
             return Ok();
         }
-
+        /// <summary>
+        /// Updates a encounter object and saves it to the database.
+        /// </summary>
+        /// <param name="encounter"></param>
+        /// <returns>IHttpActionResult</returns>
         public IHttpActionResult Put(EncounterEdit encounter)
         {
             if (!ModelState.IsValid)
@@ -60,7 +80,11 @@ namespace CloseEncounters.WebAPI.Controllers
 
 
         }
-
+        /// <summary>
+        /// Selects a certain encounter object with the inputted Encounter Id and removes it from the database.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>IHttpActionResult</returns>
         public IHttpActionResult Delete(int id)
         {
             var service = CreateEncounterService();
